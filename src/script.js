ccdbf6bd4b36160200c9e91cdee276d1d85d9f9b
Kirill Barsukov <kirillsbarsukov@gmail.com>
2019-11-29 12:13:06 -0500

    Fixed some problems

--- src/script.js
+++ src/script.js
@@ -2,8 +2,11 @@
 let url = "https://kirillsbarsukov.github.io/JSONLab/src/json.json";
 let request = new XMLHttpRequest();
 request.open("GET", request);
+
 request.responseType = 'json';
+
 request.send();
+
 request.onload = function() {
     let wierdStuff = request.response; 
     console.log(wierdStuff);
