0637b2f2f9472f8114390cea5812dd3d27cb3691
Kirill Barsukov <kirillsbarsukov@gmail.com>
2019-11-29 16:16:08 -0500

    Fixed some problems

--- src/script.js
+++ src/script.js
@@ -1,14 +1,15 @@
+// declare constants
+const url = "https://kirillsbarsukov.github.io/JSONLab/src/json.json";
+const urlImg = "https://kirillsbarsukov.github.io/JSONLab/img/";
 
-let url = "https://kirillsbarsukov.github.io/JSONLab/src/json.json";
 let request = new XMLHttpRequest();
 let header = document.querySelector('header');
+let section = document.querySelector('section');
+
 
 request.open("GET", url);
-
 request.responseType = 'json';
-
 request.send();
-
 request.onload = function() {
     let wierdStuff = request.response; 
     console.log(wierdStuff);
@@ -30,6 +31,35 @@ const loadTitles = (jsonObject) => {
 }
 
 function loadProducts (jsonObject) {
+    const products = jsonObject['products'];
+    for(let i = 0; i < products.length; i++){
+        //create a few different elements 
+        let article = document.createElement('article');
+        let h2 = document.createElement('h2');
+        let img = document.createElement('img');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let list = document.createElement('ul');
+        
+        //img.setAttribute('src', urlImg[i].image);
+        h2.textContent = products[i].productName;
+        p1.textContent = products[i].description;
+        p2.textContent = "$ " + products[i].price;
+        
+        let features = products[i].features;
+        for(let k = 0; k < features.length; k++) {
+          let listItem = document.createElement('li'); 
+          listItem.textContent = features[k];
+          list.appendChild(listItem); 
+        }
 
+
+        //article.appendChild(img); 
+        article.appendChild(h2);
+        article.appendChild(p1); 
+        article.appendChild(p2);
+        article.appendChild(list);
+        section.appendChild(article); 
+    }
 
 }
